9c37dfdafc88c1a1d5721a137d1d5e591bfdb480
Hyun Jin Kim <costa.kim@day1company.co.kr>
2022-09-29 10:41:53 +0900

    fix: print_url - mock 커맨드시에는 Cloud Run 관련 URL을 프린트 하지 않도록 수정

--- deploy/lib.sh
+++ deploy/lib.sh
@@ -411,9 +411,11 @@ _deploy() {
 }
 
 print_urls() {
-  echo "* Artifact Registry Container Image at 👉 $(get_artifact_registry_url)"
-  echo "* Cloud Run at 👉 $(get_cloudrun_url)"
-  echo "* Cloud Run Log at 👉 $(get_cloudrun_log_url)"
+  if [ "$COMMAND" == "deploy" ]; then
+    echo "* Artifact Registry Container Image at 👉 $(get_artifact_registry_url)"
+    echo "* Cloud Run at 👉 $(get_cloudrun_url)"
+    echo "* Cloud Run Log at 👉 $(get_cloudrun_log_url)"
+  fi
   echo "* Cloud Pub/Sub Topic at 👉 $(get_topic_url)"
   echo "* Cloud Pub/Sub Subscription at 👉 $(get_subscription_url)"
   if [ "$USING_SCHEDULER" = "true" ]; then
@@ -423,7 +425,7 @@ print_urls() {
 
 _undeploy() {
   # if $USING_SCHEDULER == 'true' -> create scheduler
-  if [[ "$USING_SCHEDULER" = "true" ]]; then
+  if [ "$USING_SCHEDULER" == "true" ]; then
     delete_scheduler
   fi
 
