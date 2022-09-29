0f65241e3d383415a6d2c23390237f94f4d9b4cf
Hyun Jin Kim <costa.kim@day1company.co.kr>
2022-09-29 18:06:21 +0900

    fix: 사용하지 않는 pebbles 로거 제거

--- nodejs-sample/src/index.ts
+++ nodejs-sample/src/index.ts
@@ -1,10 +1,8 @@
-import { LoggerFactory } from '@day1co/pebbles';
 import { Application } from '@day1co/cornerstone-starter';
 
 import { SampleService } from './sample-service';
 import { SampleTask } from './sample-task';
 
-const logger = LoggerFactory.getLogger('cornerstone-nodejs-sample');
 const service = new SampleService();
 const task = new SampleTask(service);
 const app = new Application(task);
